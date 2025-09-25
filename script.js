9163bf395322d476d7f5ed9bd5f63d90891dbb5d
m.bayat <minabayatt9@gmail.com>
2025-09-25 21:14:14 +0330

    v1

---
+++ script.js
@@ -0,0 +1,4 @@
+const btnEl = document.getElementById("btn")
+const modalEl = document.getElementById("modal")
+const closeEl = document.getElementById("close")
+const backdropEl = document.getElementById("backdrop")
