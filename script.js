907b3d3478905c5bae598805eb23f971082c7c7d
minabayat <minabayatt9@gmail.com>
2025-09-27 16:44:59 +0330

    v1

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const btnEl = document.getElementById("btn")
 const modalEl = document.getElementById("modal")
 const closeEl = document.getElementById("close")
 const backdropEl = document.getElementById("backdrop")
+
+btnEl.addEventListener("click", () => {
+  modalH.classList.remove("hide");
+  blurEl.classList.remove("hide");
+});
+colseEl.addEventListener("click", () => {
+  modalH.classList.add("hide");
+  blurEl.classList.add("hide");
+});
+
+backdropElEl.addEventListener("click", () => {
+  modalH.classList.add("hide");
+  blurEl.classList.add("hide");
+});
